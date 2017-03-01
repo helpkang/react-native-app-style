4e161b9c5c81517e89ca4e7a75b41847b94a4537
helpkang <helpkang@gmail.com>
2017-03-02 00:32:45 +0900

    icon add

---
+++ Icon.js
@@ -0,0 +1,7 @@
+import styledComponent from "react-native-app-style/style"
+
+import Ionicons from 'react-native-vector-icons/Ionicons'
+
+moduele.export ={
+    Ionicon: styledComponent('icon')(Ionicons)
+}
